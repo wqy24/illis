ef13c17eb4d6df31bd8e7f072daaf0495196e2bd
wwy24 <as2734@126.com>
2023-04-06 20:59:15 +0800

    Initialize

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+var http2 = require("http2");
 
+function server(port, option) {
+  return {
+    port,
+    option,
+    route(method, path, handler) {},
+    extend() {},
+    handler: {},
+    start() {}
+  }
+}
