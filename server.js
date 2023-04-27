7ab7d56d81d45648320a0256ed4f87762a88d25c
wwy24 <as2734@126.com>
2023-04-27 19:49:07 +0800

    Add "routers" object

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http2 = require("node:http2");
-import route from "router"
+import route from "router";
 
 function server(port, option) {
   return {
     port,
     option,
     route,
+    routers: {},
     extend() {},
     handler: {},
     start() {}
