6d4d1f64a19722f552c2f1d2b47ba7f247d91e0c
wwy24 <as2734@126.com>
2023-04-28 20:34:39 +0800

    remove server.js

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(method, path, handler) {
+
+}
+
+export default route;
--- test/routers.js
+++ test/routers.js
@@ -1,12 +1,36 @@
+var chai = require("chai");
 import illis from "../index";
-illis(8888, options)
-  .route(["GET"], "/", (handler) => handler.stream.respond({":status": 200, "content-type": "text/plain"}, "Hello, World!"))
-  .route(["GET"], "/hello", (handler) => handler.stream.respond({":status": 200, "content-type": "text/plain"}, "Hello, again!"))
-  .route(["*"], "/:*", (handler) => handler.stream.respond({":status": 404, "content-type": "text/plain"}, "404 Not Found"))
-  .start().then(port => {
-    console.log("Server running at ${port}");
+
+illis(8888, options).route(["GET"], "/", (handler) => {
+  handler.stream.respond({
+    ":status": 200,
+    "content-type": "text/plain"}
+  );
+  handler.stream.end("Hello World!");
+}).route(["GET"], "/hello", handler => {
+  handler.stream.respond({
+    ":status": 200,
+    "content-type": "text/plain"
   });
+  handler.stream.end("Hello Again!");
+}).route(["*"], "/:*", handler => {
+  handler.stream.respond({
+    ":status": 404,
+    "content-type": "text/plain"
+  });
+  handler.stream.end("404 Not Found");
+}).start().then(port => {
+  // console.log("Server running at ${port}");
+  test(port);
+});
 
-moudle.exports = {
+function test(port) {
+  moudule.exports = {
+    "Port": {
+      "should be 8888": () => {
+        chai.expect(port).to.equal(8888);
+      }
+    }
 
-};
+  }
+}
