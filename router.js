6c5bd4e2e414addca560c1164d8a864c790ddfe9
wwy24 <as2734@126.com>
2023-04-29 21:19:27 +0800

    Fix the bug of router validator

--- router.js
+++ router.js
@@ -3,25 +3,27 @@ function route(method, path, handler) {
     //console.warn(`Warning: invalid router "$(path)"`);
     return this;
   }
+  return
 }
 
 function valid(path) {
   var pathList = path.split("/"),
       questMarked,   //REST param matches a path or nothing (Marking: ":name?")
       paramsMarked,  //REST params                          (Marking: ":name ")
-      starred;       //REST param matches any more path     (Marking: ":*    ")
+      starred,       //REST param matches any more path     (Marking: ":*    ")
+      result = true;
 
   pathList.forEach(path => {
     if (!(/^[A-Za-z_\-0-9:](\* | ([A-Za-z0-9\-_]*\??))$/.test(path))) {
-      return false;
+      result = false
     } else if (/^\:/.test(path)) {
       if (/^\/.+/)
-        return false;
+        result = false;
 
       paramsMarked = true;
 
       if(starred || questMarked)
-        return false; //No params after star param and question param
+        result = false; //No params after star param and question param
 
       if (/^\:\*/.test(path))
         starred = true;
@@ -29,11 +31,11 @@ function valid(path) {
         questMarked = true;
 
     } else if (paramsMarked) {
-      return false;
+      result = false;
     }
   });
 
-  return true;
+  return result;
 }
 
 export default route;
